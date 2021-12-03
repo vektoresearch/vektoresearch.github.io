b3960be8fde3aef4cd4817a60a3fce81fc9e2494
Vektor Research <vektoresearch@gmail.com>
2021-12-03 14:46:06 +0700

    change to postgres

--- myproject/__init__.py
+++ myproject/__init__.py
@@ -11,8 +11,7 @@ app = Flask(__name__)
 
 # User Database
 app.config['SECRET_KEY'] = 'jue878215fd84;wndjhd3829e82!@@#%!^!&'
-basedir = os.path.abspath(os.path.dirname(__file__))
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///'+os.path.join(basedir,'data.sqlite')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
